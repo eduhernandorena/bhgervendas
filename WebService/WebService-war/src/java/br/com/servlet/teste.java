2d5cfcaa974023f0d6397a7c256a3c0f8066faaf
eduuardobh <eduuardobh@2b3ba4f8-ac5e-f7a2-c0ac-6efa0d120cdf>
2012-08-21 00:26:52 +0000

    Foram modificados os arquivos de configuração do projeto Web para conectar com o Glassfish.

--- WebService/WebService-war/src/java/br/com/servlet/teste.java
+++ WebService/WebService-war/src/java/br/com/servlet/teste.java
@@ -1,7 +1,7 @@
 package br.com.servlet;
 
-import br.com.ejb.bean.UF;
-import br.com.ejb.ejb.UfDAOLocal;
+import br.com.ejb.bean.Usuario;
+import br.com.ejb.ejb.UsuarioDAORemote;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.ejb.EJB;
@@ -19,31 +19,18 @@ import javax.servlet.http.HttpServletResponse;
 public class teste extends HttpServlet {
 
     @EJB
-    private UfDAOLocal ufdao;
+    private UsuarioDAORemote userdao;
 
-    /**
-     * Processes requests for both HTTP
-     * <code>GET</code> and
-     * <code>POST</code> methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
         try {
-            /*
-             * TODO output your page here. You may use following sample code.
-             */
-            UF uf = new UF();
-            uf.setCodIbge(1L);
-            uf.setDescricao("teste");
-            uf.setSigla("TT");
-            ufdao.create(uf);
+
+            Usuario user = new Usuario();
+            user.setNome("eduardo");
+            user.setSenha("tcc");
+            userdao.create(user);
             out.println("<html>");
             out.println("<head>");
             out.println("<title>Servlet teste ae ó</title>");
@@ -56,45 +43,4 @@ public class teste extends HttpServlet {
             out.close();
         }
     }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP
-     * <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Handles the HTTP
-     * <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
 }
